de74ae9b89bc512d2bf1092b03ddcc33a0aa7ef4
splitthinker <49044576+KumarGaurav13@users.noreply.github.com>
2026-03-03 22:15:33 +0530

    Changes for counter program

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,68 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [countToSet, setCountToSet] = useState(0);
 
+  // const incrementHandler = (currVal) => {
+  //   // setCount (currVal + 1); //shorthand notation
+  //   setCount((preValue) => preValue+ currVal + 1);
+  //   setCount((preValue) => preValue + 1);
+  //   setCount((preValue) => preValue + 1);
+  // }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      
+      <h1>Counter is {count}</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+          Count is {count}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div>
+        <button 
+          onClick={() => setCount(count+1)} 
+          style={{margin: "0 5px"}}
+        >
+          Increment
+        </button>
+        <button 
+          onClick={() => setCount(Math.max(count-1, 0))} 
+          style={{margin: "0 5px"}}
+        >
+          Decrement
+        </button>
+        <button 
+          onClick={() => setCount((count)=> 0)} 
+          style={{margin: "0 5px"}}
+        >
+          Reset
+        </button>
+      </div>
+
+      <div style={{margin: "20px 0"}}>
+        <input 
+          style={{
+            width: "100px",
+            border: "1px solid gray",
+            margin: "0 5px",
+            padding: "0.6em 1.2em"
+          }}
+          value={countToSet}
+          onChange={(e) => {setCountToSet(e.target.value)}}
+          type='text'
+        />
+        <button 
+          onClick={() => {
+            setCount((count) => Number(countToSet));
+            setCountToSet(0);
+          }} 
+          style={{margin: "0 5px"}}
+        >
+          Set to {countToSet}
+        </button>
+      </div>
+      
     </>
   )
 }
